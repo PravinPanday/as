ac3e3bc88d47daf3a40efad0963eb0eb47eedd16
PravinPanday <007pravinpandey@gmail.com>
2024-06-16 08:26:44 -0700

    Delete src directory

--- src/utils/formateDate.js
+++
@@ -1,6 +0,0 @@
-
-export const formateDate =(Date, config)=> {
-    const defaultOptions = {day :'numeric', month:'short' ,year: 'numeric' }
-    const options = config ? config : defaultOptions
-    return new Date(Date).tolocalDateString('en-US', options)
-}
